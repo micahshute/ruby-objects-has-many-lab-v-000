5e3769fa3f8ec1573dd0ad281fbcf4734c2bf5ae
Micah Shute <micah.shute@gmail.com>
2018-07-04 07:57:19 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+
+  @@all = []
+  def self.all
+    @@all
+  end
+  
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  
+  
+end
